0f84e6dfcde718b354776de2745fab25d947fbac
Anthony <flexedoyster@hotmail.com>
2014-11-18 20:14:05 -0500

    Revert "Page 1 Done"
    This reverts commit 9de7a8193e811ce2aedbe97e2a41ca8d0ebacd19.

--- Assets/Class/PlayerClass.cs
+++ Assets/Class/PlayerClass.cs
@@ -2,8 +2,7 @@
 using System.Collections;
 
 public class PlayerClass : MonoBehaviour {
-	public float speed;
-	public GameObject bombPrefab;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -11,16 +10,6 @@ public class PlayerClass : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (transform.position.x > 18) {
-			//get new speed
-			speed = Random.Range(8f,12f);
-			transform.position = new Vector3( -18f, transform.position.y, transform.position.z );
-		}		
-		transform.Translate(0, 0, speed * Time.deltaTime);
-
-		if (Input.anyKeyDown) {
-			GameObject bombObject = (GameObject)Instantiate(bombPrefab);
-			bombObject.transform.position = this.gameObject.transform.position;
-		}
+	
 	}
 }
