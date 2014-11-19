9de7a8193e811ce2aedbe97e2a41ca8d0ebacd19
Anthony <flexedoyster@hotmail.com>
2014-11-18 20:12:24 -0500

    Page 1 Done

---
+++ Assets/Class/BombClass.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombClass : MonoBehaviour {
+	private float ySpeed = -4f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate( new Vector3(0f, ySpeed*Time.deltaTime, 0f) );
+		if (transform.position.y < -11) {
+			Destroy(this.gameObject);
+		}
+	}
+	
+	void OnTriggerEnter(Collider obj) {
+		if (obj.gameObject.name == "Shark") {
+			//reset shark
+			obj.gameObject.transform.rotation = Quaternion.identity;
+			obj.gameObject.transform.position = new Vector3(20f, -3f, 8f);
+			Destroy(this.gameObject);
+		}
+	}
+}
+
