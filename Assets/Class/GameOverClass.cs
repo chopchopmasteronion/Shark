7b760afdf464c2a28f65a957e58841e2494b83c2
SinkingShipMedia <sinkingshipmediagroup@gmail.com>
2014-11-24 19:12:25 -0500

    Revert f57a96f..b6ffaa8
    This rolls back to commit f57a96f8c33427cd133e110b9e40fe65315132b3.

---
+++ Assets/Class/GameOverClass.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverClass : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown) {
+			PlayerClass.score = 0;
+			Application.LoadLevel("scene");
+		}
+	}
+}
